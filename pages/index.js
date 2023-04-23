afa4d42c1170499ce8f989600c65db7a237c972b
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2023-04-23 20:37:00 +0300

    feat: starter code setup

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Todo</main>
+    </div>
+  );
+}
