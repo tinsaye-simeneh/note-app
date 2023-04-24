6eff3d2a94a20bc82e0f8cd17175c62d0f8bc9f5
Tinsaye Simeneh <64516393+tinsaye-simeneh@users.noreply.github.com>
2023-04-24 19:51:58 +0300

    feat: minor style update

--- pages/list/mynotes.tsx
+++ pages/list/mynotes.tsx
@@ -30,7 +30,7 @@ function myNotes() {
   return (
     <Container size={1000} className={classes.wrapper}>
       {" "}
-      {/* This is the container that holds the title and button */}
+      {/* This is the container that holds the Page title and button */}
       <Group position="center" p="lg" bg="green">
         <Group>
           <Title>My Notes</Title>
@@ -41,6 +41,7 @@ function myNotes() {
           </Button>
         </Group>
       </Group>
+      {/* This is the container that holds the note title and button */}
       <Group position="left" p="lg" bg="green" my="lg">
         <Group>
           <IconNotes size={30} color="white" />
